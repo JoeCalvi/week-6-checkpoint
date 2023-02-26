28a78c61a9ad2a32d8d7411cd460ce455f1b393d
JoeCalvi <joecalvi@me.com>
2023-02-26 11:20:59 -0700

    profile results in component and drawing

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -1,20 +1,44 @@
 <template>
     <div class="ProfileCard">
-        
-
+        <div class="row justify-content-center my-3">
+            <div class="col-md-3" v-for="r in results">
+                <div v-if="results">
+                    <div  class="card mb-2">
+                        <div class="card-body text-center">
+                            <img :src="r.picture" class="img-fluid creator-picture rounded-circle" alt="">
+                            <div class="my-2">
+                                <h5>{{ r.name }}</h5>
+                                <p>{{ r.bio }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
-
 <script>
+import { computed } from "vue";
+import { AppState } from "../AppState.js";
+
 export default {
     setup(){
-        return {}
+        return {
+            results: computed(() => AppState.results)
+        }
     }
 }
 </script>
 
 
 <style lang="scss" scoped>
+
+.creator-picture{
+  height: 15vw;
+  width: 15vw;
+  object-fit: cover;
+  object-position: center;
+}
 
 </style>
--- src/pages/SearchResultsPage.vue
+++ src/pages/SearchResultsPage.vue
@@ -1,20 +1,33 @@
 <template>
-    <div class="SearchPage">
+    <div v-if="results" class="SearchPage">
 
+        <ProfileCard :results="results"/>
 
     </div>
 </template>
 
 
 <script>
+import { computed } from "vue";
+import { AppState } from "../AppState.js";
+
 export default {
     setup(){
-        return {}
+        return {
+            results: computed(() => AppState.results)
+        }
     }
 }
 </script>
 
 
 <style lang="scss" scoped>
+
+.creator-picture{
+  height: 15vw;
+  width: 15vw;
+  object-fit: cover;
+  object-position: center;
+}
 
 </style>
